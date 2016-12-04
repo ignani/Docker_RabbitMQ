75ae608407939beb3bb078559963f31244c673ad
Babu Reddy <babureddyh@hotmail.com>
2016-12-04 23:42:45 +0530

    Added example 2

---
+++ WorkQueues/WorkQueuesSend/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace WorkQueuesSend
+{
+  /// <summary>
+  ///   Hello World project has been modified here. It used to just send a "Hello World!" message into the queue named
+  ///   "hello".
+  /// </summary>
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      var factory = new ConnectionFactory {HostName = "localhost"};
+      using (var connection = factory.CreateConnection())
+      using (var channel = connection.CreateModel())
+      {
+        channel.QueueDeclare("hello", false, false, false, null);
+
+        var message = "Hello World!";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish("", "hello", null, body);
+        Console.WriteLine(" [x] Sent {0}", message);
+      }
+
+      Console.WriteLine(" Press [enter] to exit.");
+      Console.ReadLine();
+    }
+  }
+}
