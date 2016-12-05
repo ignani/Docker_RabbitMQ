e8baee5a82f6618600de49a48a417104b3a97d49
Babu Reddy <babureddyh@hotmail.com>
2016-12-05 21:13:22 +0530

    Updated the code to use routing key. The messages are dispatched to all the receivers in a round-robin mode.

--- WorkQueues/WorkQueuesSend/Program.cs
+++ WorkQueues/WorkQueuesSend/Program.cs
@@ -4,29 +4,42 @@ using RabbitMQ.Client;
 
 namespace WorkQueuesSend
 {
-  /// <summary>
-  ///   Hello World project has been modified here. It used to just send a "Hello World!" message into the queue named
-  ///   "hello".
-  /// </summary>
-  internal class Program
-  {
-    private static void Main(string[] args)
-    {
-      var factory = new ConnectionFactory {HostName = "localhost"};
-      using (var connection = factory.CreateConnection())
-      using (var channel = connection.CreateModel())
+   /// <summary>
+   ///    Hello World project has been modified here. User can send the message as a parameter.
+   ///    The number of .(dots) at the end of the message determines the number of seconds the threat has to put to sleep.
+   ///   The queue name and the routing key name should be the same since we are using the default exchange.
+   /// </summary>
+   internal class Program
+   {
+      private static void Main(string[] args)
       {
-        channel.QueueDeclare("hello", false, false, false, null);
+         var factory = new ConnectionFactory {HostName = "localhost"};
+         using (var connection = factory.CreateConnection())
+         using (var channel = connection.CreateModel())
+         {
+            channel.QueueDeclare("task_queue", false, false, false, null);
 
-        var message = "Hello World!";
-        var body = Encoding.UTF8.GetBytes(message);
+            var message = GetMessage(args);
+            for (int x = 1; x <= 50; x++)
+            {
+               var body = Encoding.UTF8.GetBytes(message + " = " + x + "..");
 
-        channel.BasicPublish("", "hello", null, body);
-        Console.WriteLine(" [x] Sent {0}", message);
+               var properties = channel.CreateBasicProperties();
+               properties.Persistent = true;
+
+               channel.BasicPublish("", "task_queue", properties, body);
+            }
+            Console.WriteLine(" [x] Sent {0}", message);
+         }
+
+         Console.WriteLine(" Press [enter] to exit.");
+         Console.ReadLine();
       }
 
-      Console.WriteLine(" Press [enter] to exit.");
-      Console.ReadLine();
-    }
-  }
+      //returns the message passed through args. If args is null, returns default message.
+      private static string GetMessage(string[] args)
+      {
+         return args.Length > 0 ? string.Join(" ", args) : "Hello World!";
+      }
+   }
 }
